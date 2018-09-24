dbbe42e4d612c4e812098d7e82fe1e7e6bccbdd4
Graeme T. Lloyd <graemetlloyd@gmail.com>
2018-09-24 15:14:12 +0100

    Updated MRPCollapse to new matrix format.

--- R/MRPCollapse.R
+++ R/MRPCollapse.R
@@ -22,46 +22,46 @@
 MRPCollapse <- function(clad.matrix) {
   
   # Find any constant characters:
-  constantcharacters <- which(unlist(lapply(lapply(split(clad.matrix$matrix, rep(1:ncol(clad.matrix$matrix), each = nrow(clad.matrix$matrix))), unique), length)) < 2)
+  constantcharacters <- which(unlist(lapply(lapply(split(clad.matrix$Matrix_1$Matrix, rep(1:ncol(clad.matrix$Matrix_1$Matrix), each = nrow(clad.matrix$Matrix_1$Matrix))), unique), length)) < 2)
   
   # Prune these if found:
   if(length(constantcharacters) > 0) clad.matrix <- MatrixPruner(clad.matrix, characters2prune = constantcharacters)
   
   # Only continue if matrix still has at least one column:
-  if(ncol(clad.matrix$matrix) > 0) {
+  if(ncol(clad.matrix$Matrix_1$Matrix) > 0) {
     
     # Find any zero-weight characters:
-    zeroweightcharacters <- which(clad.matrix$weights == 0)
+    zeroweightcharacters <- which(clad.matrix$Matrix_1$Weights == 0)
     
     # Prune these if found:
     if(length(zeroweightcharacters) > 0) clad.matrix <- MatrixPruner(clad.matrix, characters2prune = zeroweightcharacters)
     
     # Only continue if matrix still has at least one column:
-    if(ncol(clad.matrix$matrix) > 0) {
+    if(ncol(clad.matrix$Matrix_1$Matrix) > 0) {
       
       # Check for step matrices and stop if found:
-      if(!is.null(clad.matrix$step.matrices[[1]][1])) stop("Function not designed to work with step matrices.")
+      if(!is.null(clad.matrix$Topper$StepMatrices)) stop("Function not designed to work with step matrices.")
       
       # Check for non-binary characters and stop if found:
-      if(length(unique(as.vector(clad.matrix$matrix))) != 2) stop("Function not designed for non-binary data.")
+      if(length(unique(as.vector(clad.matrix$Matrix_1$Matrix))) != 2) stop("Function not designed for non-binary data.")
       
       # Store taxon names:
-      taxon.names <- rownames(clad.matrix$matrix)
+      taxon.names <- rownames(clad.matrix$Matrix_1$Matrix)
       
       # Collapse cladistic matrix to just unique characters:
-      clad.matrix$matrix <- matrix(unlist(strsplit(sort(apply(clad.matrix$matrix, 2, paste, collapse = ""))[!duplicated(sort(apply(clad.matrix$matrix, 2, paste, collapse = "")))], "")), nrow = length(taxon.names), dimnames = list(taxon.names, c()))
+      clad.matrix$Matrix_1$Matrix <- matrix(unlist(strsplit(sort(apply(clad.matrix$Matrix_1$Matrix, 2, paste, collapse = ""))[!duplicated(sort(apply(clad.matrix$Matrix_1$Matrix, 2, paste, collapse = "")))], "")), nrow = length(taxon.names), dimnames = list(taxon.names, c()))
       
       # Overwrite ordering to new length:
-      clad.matrix$ordering <- rep("unord", ncol(clad.matrix$matrix))
+      clad.matrix$Matrix_1$Ordering <- rep("unord", ncol(clad.matrix$Matrix_1$Matrix))
       
       # Overwrite weights to new length:
-      clad.matrix$weights <- rep(1, ncol(clad.matrix$matrix))
+      clad.matrix$Matrix_1$Weights <- rep(1, ncol(clad.matrix$Matrix_1$Matrix))
       
       # Overwrite max vals to new length:
-      clad.matrix$max.vals <- rep(1, ncol(clad.matrix$matrix))
+      clad.matrix$Matrix_1$MaxVals <- rep(1, ncol(clad.matrix$Matrix_1$Matrix))
       
       # Overwrite min vals to new length:
-      clad.matrix$min.vals <- rep(0, ncol(clad.matrix$matrix))
+      clad.matrix$Matrix_1$MinVals <- rep(0, ncol(clad.matrix$Matrix_1$Matrix))
       
     }
     
