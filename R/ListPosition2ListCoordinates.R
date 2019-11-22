b20ec8b3b9e19ed3586b97f9a16b3be5fd02aa69
Graeme T. Lloyd <graemetlloyd@gmail.com>
2019-11-22 10:43:53 +0000

    Built proper manual for ListPosition2ListCoordinates.

--- R/ListPosition2ListCoordinates.R
+++ R/ListPosition2ListCoordinates.R
@@ -1,18 +1,28 @@
-#' Gives position of the kth item in a list
+#' Gives the exact position of the kth item in a list
 #' 
-#' Gives position of the kth item in a list of vectors.
+#' @description
+#'
+#' Gives the exact position of the kth item in a list comprised of vectors.
 #' 
 #' @param Position The kth value for which the position is desired.
 #' @param ListDimensions The dimensions (vector length) of each part of the list in order.
 #'
-#' @return The position, in tersm of both list number ($ListNumber) and list position ($ListPosition), of the kth value.
+#' @details
 #'
-#' @author Graeme T. Lloyd \email{graemetlloyd@@gmail.com}
+#' The R language has a great variety of ways that data can be stored, but sometimes it can be hard to isolate the required information. For example, imagine you store data in a list composed of vectors of length 1, 3, 2, 6, 1, and 3, but you just want to know where you will find the 13th value. This function will give "coordinates" for that value corresponding to the list number (5 in the example) and vector position in that list (1 in the example).
+#'
+#' @return
+#'
+#' The position, in terms of both list number ($ListNumber) and list position ($ListPosition), of the kth value.
+#'
+#' @author
+#'
+#' Graeme T. Lloyd \email{graemetlloyd@@gmail.com}
 #'
 #' @examples
 #'
 #' # Find the position of the 13th value:.
-#' ListPosition2ListCoordinates(13, c(1,3,2,6,1,3))
+#' ListPosition2ListCoordinates(13, c(1, 3, 2, 6, 1, 3))
 #'
 #' @export ListPosition2ListCoordinates
 ListPosition2ListCoordinates <- function(Position, ListDimensions) {
